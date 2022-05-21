e29e1bdc185514da0e06ffa963dffaa59a82ada1
jinquan chen <jinqchen@cs.stonybrook.edu>
2022-05-21 05:08:21 -0400

    mark with link

--- map.py
+++ map.py
@@ -4,9 +4,17 @@ data = pandas.read_csv("volcanoes.txt")
 lat=list(data["LAT"])
 lon=list(data["LON"])
 name=list(data["NAME"])
-map=folium.Map(location=[40.916908965758296, -73.12485183064408],title="Stamen Terrain")
+elev=list(data['ELEV'])
+html = """
+Volcano name:<br> 
+<a href="https://www.google.com/search?q=%s\" target="_blank">%s</a><br>
+Height: %s m
+"""
+
+map=folium.Map(location=[40, -73],title="Stamen Terrain")
 fg=folium.FeatureGroup(name="My Map")
-for la,ln,nm in zip(lat,lon,name):
-    fg.add_child(folium.Marker(location=[la, ln],popup=nm,icon=folium.Icon(color="green")))
+for la,ln,name,el in zip(lat,lon,name,elev):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[la, ln],popup=folium.Popup(iframe),icon=folium.Icon(color="green")))
     map.add_child(fg)
 map.save("Map1.html")
